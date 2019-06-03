8661686f34cec1da8b852db950e65cb30201a8ec
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 16:17:46 +0300

    version 0.2

--- zuZsl/test/query_demand.jl
+++ zuZsl/test/query_demand.jl
@@ -6,13 +6,10 @@ d1 = Date(2018, 05, 01)
 d2 = Date(2018, 05, 01)
 demand = Caiso.caiso_query_demand(d1, d2, verbose = true)[1]
 
-@testset "assertions" begin
+@testset "logic" begin
 
     test_1_d1 = Date(2018, 05, 02)
     test_1_d2 = Date(2018, 05, 01)
-    @test_throws AssertionError Caiso.caiso_query_demand(test_1_d1, test_1_d2)
-
-    test_1_d2 = Date(2018, 04, 09)
-    @test_throws AssertionError Caiso.caiso_query_demand(test_1_d1, test_1_d2)
+    @test_throws DataType Caiso.caiso_query_demand(test_1_d1, test_1_d2)
 
 end
