6148c2cedd6dd99c038b36bffdf6433dd03ee7b8
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 16:04:08 +0300

    version 0.1

--- test/query_demand.jl
+++ test/query_demand.jl
@@ -3,7 +3,7 @@ Pkg.add(PackageSpec(url="https://github.com/antonhinneck/Caiso.jl.git"))
 using Caiso, Dates, Test
 
 d1 = Date(2018, 05, 01)
-d2 = Date(2018, 05, 02)
+d2 = Date(2018, 05, 01)
 demand = Caiso.caiso_query_demand(d1, d2, verbose = true)[1]
 
 @testset logic begin
