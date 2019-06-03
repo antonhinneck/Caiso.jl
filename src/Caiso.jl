00c0d2fdbc7b82a3624a4386b1a31fd4794ebac7
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 17:03:11 +0300

    version 0.2

--- src/Caiso.jl
+++ src/Caiso.jl
@@ -8,81 +8,81 @@ __precompile__(true)
 
 # REQUIRES: Dates, HTTP, CSV, Dates
 #--------------------------------------
-        using HTTP, JSON, CSV, DataFrames, Dates
+using HTTP, JSON, CSV, DataFrames, Dates
 
 # QUERIES -----------------------------------------
 #--------------------------------------------------
 
-        function caiso_query_demand(day_1::Date,
-                                    day_2::Date;
-                                    verbose = false)
+function caiso_query_demand(day_1::Date,
+                            day_2::Date;
+                            verbose = false)
 
-                @assert (day_1 <= day_2) "Day 1 <= Day 2"
-                @assert (day_1 >= Date(2018, 04, 10)) "First entry was sampled on April 10th, 2018"
+        @assert (day_1 <= day_2) "Day 1 <= Day 2"
+        @assert (day_1 >= Date(2018, 04, 10)) "First entry was sampled on April 10th, 2018"
 
-                url_root::String = "http://www.caiso.com/outlook/SP/history/"
-                url_end::String = "/demand.csv"
-                url::String = ""
+        url_root::String = "http://www.caiso.com/outlook/SP/history/"
+        url_end::String = "/demand.csv"
+        url::String = ""
 
-                current_date::Date = day_1
+        current_date::Date = day_1
 
-                current_date_year_string::String = ""
-                current_date_month_string::String = ""
-                current_date_day_string::String = ""
+        current_date_year_string::String = ""
+        current_date_month_string::String = ""
+        current_date_day_string::String = ""
 
-                counter = 1
-                net_demand = Vector{Array{Int64, 1}}()
-                dates = Vector{Dates.Date}()
+        counter = 1
+        net_demand = Vector{Array{Int64, 1}}()
+        dates = Vector{Dates.Date}()
 
-                while current_date != day_2 + Dates.Day(1)
+        while current_date != day_2 + Dates.Day(1)
 
-                        # Process Year
-                        #-------------
-                        current_date_year_string = string(year(current_date))
+                # Process Year
+                #-------------
+                current_date_year_string = string(year(current_date))
 
-                        # Process Month
-                        #--------------
-                        if month(current_date) < 10
-                                current_date_month_string = string(0) * string(month(current_date))
-                        else
-                                current_date_month_string = string(month(current_date))
-                        end
-
-                        # Process Day
-                        #------------
-                        if day(current_date) < 10
-                                current_date_day_string = string(0) * string(day(current_date))
-                        else
-                                current_date_day_string = string(day(current_date))
-                        end
-
-                        # Build GET URL
-                        #--------------
-                        url =   url_root *
-                                current_date_year_string *
-                                current_date_month_string *
-                                current_date_day_string *
-                                url_end
-
-                        data = HTTP.request("GET", url, [])
-                        data_df = dropmissing(CSV.read(data.body))
-
-                        push!(net_demand, data_df[:, 4])
-                        push!(dates, current_date)
-
-                        current_date += Dates.Day(1)
-
-                        # Output
-                        #-------
-                        if verbose
-                                print("\nAPI REQUEST PROCESSED:\n")
-                                print(url,"\n")
-                        end
-
+                # Process Month
+                #--------------
+                if month(current_date) < 10
+                        current_date_month_string = string(0) * string(month(current_date))
+                else
+                        current_date_month_string = string(month(current_date))
                 end
 
-                return net_demand, dates
+                # Process Day
+                #------------
+                if day(current_date) < 10
+                        current_date_day_string = string(0) * string(day(current_date))
+                else
+                        current_date_day_string = string(day(current_date))
+                end
+
+                # Build GET URL
+                #--------------
+                url =   url_root *
+                        current_date_year_string *
+                        current_date_month_string *
+                        current_date_day_string *
+                        url_end
+
+                data = HTTP.request("GET", url, [])
+                data_df = dropmissing(CSV.read(data.body))
+
+                push!(net_demand, data_df[:, 4])
+                push!(dates, current_date)
+
+                current_date += Dates.Day(1)
+
+                # Output
+                #-------
+                if verbose
+                        print("\nAPI REQUEST PROCESSED:\n")
+                        print(url,"\n")
+                end
+
         end
+
+        return net_demand, dates
+end
 
 # Module's end
 #-------------
--- test/query_demand.jl
+++ test/query_demand.jl
@@ -1,6 +1,5 @@
-using Pkg
+using Pkg, Caiso, Dates, Test
 Pkg.add(PackageSpec(url="https://github.com/antonhinneck/Caiso.jl.git"))
-using Caiso, Dates, Test
 
 d1 = Date(2018, 05, 01)
 d2 = Date(2018, 05, 01)
