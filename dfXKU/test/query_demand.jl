db2878e756213c61383bc5ccfa7d6c54f7b645c9
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 16:12:06 +0300

    version 0.2

--- dfXKU/test/query_demand.jl
+++ dfXKU/test/query_demand.jl
@@ -6,10 +6,10 @@ d1 = Date(2018, 05, 01)
 d2 = Date(2018, 05, 01)
 demand = Caiso.caiso_query_demand(d1, d2, verbose = true)[1]
 
-@testset "logic" begin
+@testset logic begin
 
     test_1_d1 = Date(2018, 05, 02)
     test_1_d2 = Date(2018, 05, 01)
-    @test_throws DataType Caiso.caiso_query_demand(test_1_d1, test_1_d2)
+    @test_throws ArgumentError Caiso.caiso_query_demand(test_1_d1, test_1_d1)
 
 end
