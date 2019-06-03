fb2221a07acc8c72f7bfa410c4cdf77642cca396
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 16:01:48 +0300

    version 0.1

--- SBFQp/test/query_demand.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-Pkg.add(PackageSpec(url="https://github.com/antonhinneck/Caiso.jl.git"))
-using Caiso, Dates, Test
-
-d1 = Date(2018, 05, 01)
-d2 = Date(2018, 05, 02)
-demand = Caiso.caiso_query_demand(d1, d2, verbose = true)[1]
