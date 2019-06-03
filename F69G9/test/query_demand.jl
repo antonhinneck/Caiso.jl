16b53ffebe9d4af7abdcb1f45c23e9e43a7ae0e0
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-03 15:47:00 +0300

    version 0.1

--- F69G9/test/query_demand.jl
+++
@@ -1,7 +0,0 @@
-using Test, Pkg
-Pkg.add(PackageSpec(url="https://github.com/antonhinneck/Caiso.jl.git"))
-using Caiso
-
-d1 = Date(2018, 05, 01)
-d2 = Date(2019, 05, 02)
-demand = caiso_query_demand(d1, d2, true)[1]
